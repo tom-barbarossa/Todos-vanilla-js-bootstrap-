abf5db08cd470f52058fef95caa5db8f99e6a9d8
Tomasz Leszczynski <tomasz.ii.leszczynski@gmail.com>
2020-06-07 19:11:57 +0200

    WIP

--- index.js
+++ index.js
@@ -10,22 +10,32 @@ const todos =
       return Object.assign(res, { [key]: val });
     }
   }, {}).todos || [];
-let todoIndex = todos.length ? Math.max(...todos.map(e => e.id.split('-')[1])) + 1 : 0;
+let todoIndex = todos.length
+  ? Math.max(...todos.map((e) => e.id.split('-')[1])) + 1
+  : 0;
+let onlyInProgressTodos = true;
 
 function listTodos(searchPhrase) {
   let todosTemp = [...todos];
 
   if (searchPhrase) {
-    todosTemp = todosTemp.filter((e) => e.text.toLowerCase().includes(searchPhrase));
+    todosTemp = todosTemp.filter((e) =>
+      e.text.toLowerCase().includes(searchPhrase)
+    );
+  }
+
+  if (onlyInProgressTodos) {
+    todosTemp = todosTemp.filter((e) => !e.completed);
+    document.querySelector('#done-header').classList.add('d-none');
+  }
+
+  if (todosTemp.some((e) => e.completed) && !onlyInProgressTodos) {
+    document.querySelector('#done-header').classList.remove('d-none');
   }
 
   todoList.innerHTML = '';
   doneList.innerHTML = '';
   field.value = '';
-
-  if (todosTemp.some((e) => e.completed)) {
-    document.querySelector('#done-header').classList.remove('d-none');
-  }
 
   todosTemp.forEach((e) => {
     const classesToAdd = ['list-group-item', 'my-1'];
@@ -45,7 +55,9 @@ function listTodos(searchPhrase) {
       finishBtn.type = 'button';
       finishBtn.classList.add('btn', 'btn-success', 'btn-sm', 'float-right');
       finishBtn.addEventListener('click', () => {
-        const idx = todosTemp.findIndex((e) => e.id === finishBtn.parentNode.id);
+        const idx = todosTemp.findIndex(
+          (e) => e.id === finishBtn.parentNode.id
+        );
         if (idx > -1) todos[idx].completed = true;
         document.cookie = `todos=${JSON.stringify(todos)}`;
         listTodos();
@@ -63,6 +75,7 @@ const field = document.querySelector('#todo-input-field');
 const todoList = document.querySelector('#todo-list');
 const doneList = document.querySelector('#done-list');
 const searchField = document.querySelector('#search-field');
+const completedCheckbox = document.querySelector('#completed-checkbox');
 
 button.addEventListener('click', (e) => {
   todos.push({
@@ -79,5 +92,10 @@ field.addEventListener('keyup', (e) =>
 );
 
 searchField.addEventListener('input', (e) => listTodos(e.target.value));
+
+completedCheckbox.addEventListener('change', (e) => {
+  onlyInProgressTodos = !e.target.checked;
+  listTodos();
+});
 
 listTodos();
