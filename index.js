fc2405145d91beeed2c50c3f29fe1758007f94c7
Tomasz Leszczynski <tomasz.ii.leszczynski@gmail.com>
2020-06-07 18:45:18 +0200

    WIP

--- index.js
+++ index.js
@@ -1,25 +1,33 @@
-const todos = document.cookie
-.split(';')
-.reduce((res, c) => {
-  const [key, val] = c.trim().split('=').map(decodeURIComponent)
-  const allNumbers = str => /^\d+$/.test(str);
-  try {
-    return Object.assign(res, { [key]: allNumbers(val) ?  val : JSON.parse(val) })
-  } catch (e) {
-    return Object.assign(res, { [key]: val })
-  }
-}, {}).todos || [];
-let todoIndex = 0;
+const todos =
+  document.cookie.split(';').reduce((res, c) => {
+    const [key, val] = c.trim().split('=').map(decodeURIComponent);
+    const allNumbers = (str) => /^\d+$/.test(str);
+    try {
+      return Object.assign(res, {
+        [key]: allNumbers(val) ? val : JSON.parse(val),
+      });
+    } catch (e) {
+      return Object.assign(res, { [key]: val });
+    }
+  }, {}).todos || [];
+let todoIndex = todos.length ? Math.max(...todos.map(e => e.id.split('-')[1])) + 1 : 0;
 
-function listTodos() {
+function listTodos(searchPhrase) {
+  let todosTemp = [...todos];
+
+  if (searchPhrase) {
+    todosTemp = todosTemp.filter((e) => e.text.toLowerCase().includes(searchPhrase));
+  }
+
   todoList.innerHTML = '';
   doneList.innerHTML = '';
   field.value = '';
-  if (todos.some((e) => e.completed)) {
+
+  if (todosTemp.some((e) => e.completed)) {
     document.querySelector('#done-header').classList.remove('d-none');
   }
 
-  todos.forEach((e) => {
+  todosTemp.forEach((e) => {
     const classesToAdd = ['list-group-item', 'my-1'];
     const newEl = document.createElement('li');
     const newElText = document.createElement('span');
@@ -37,9 +45,9 @@ function listTodos() {
       finishBtn.type = 'button';
       finishBtn.classList.add('btn', 'btn-success', 'btn-sm', 'float-right');
       finishBtn.addEventListener('click', () => {
-        const idx = todos.findIndex((e) => e.id === finishBtn.parentNode.id);
+        const idx = todosTemp.findIndex((e) => e.id === finishBtn.parentNode.id);
         if (idx > -1) todos[idx].completed = true;
-        document.cookie = `todos=${JSON.stringify(todos)}`
+        document.cookie = `todos=${JSON.stringify(todos)}`;
         listTodos();
       });
       newEl.appendChild(finishBtn);
@@ -54,19 +62,22 @@ const button = document.querySelector('#todo-input-button');
 const field = document.querySelector('#todo-input-field');
 const todoList = document.querySelector('#todo-list');
 const doneList = document.querySelector('#done-list');
+const searchField = document.querySelector('#search-field');
 
-button.addEventListener('click', () => {
+button.addEventListener('click', (e) => {
   todos.push({
     text: field.value,
     completed: false,
     id: `todo-${todoIndex++}`,
   });
-  document.cookie = `todos=${JSON.stringify(todos)}`
+  document.cookie = `todos=${JSON.stringify(todos)}`;
   listTodos();
 });
 
 field.addEventListener('keyup', (e) =>
   e.keyCode == 13 ? button.click() : null
 );
+
+searchField.addEventListener('input', (e) => listTodos(e.target.value));
 
 listTodos();
