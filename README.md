69d7fa7a427755ca19aafecafe12783ab284fc66
Tomasz Leszczynski <tomasz.ii.leszczynski@gmail.com>
2020-08-20 13:55:05 +0200

    feat: adds redme and moves index.html to views

---
+++ README.md
@@ -0,0 +1,3 @@
+A simple Express TODO app utilizing Bootstrap.
+
+The app is available at: http://todo.tom-barbarossa.com
--- app.js
+++ app.js
@@ -4,7 +4,9 @@ const app = express();
 
 app.use(express.static('public'));
 
-app.get('/', (req, res) => res.redirect('/html/index.html'));
+app.get('/', (req, res) =>
+  res.sendFile('/views/index.html', { root: __dirname })
+);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
